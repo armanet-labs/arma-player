2dce36b25ada5a6fdf107d738999a92181ed0db6
Klaus Silveira <contact@klaussilveira.com>
2025-06-03 13:20:13 -0400

    Added outstream player for Prebid.js.

--- src/browser.js
+++ src/browser.js
@@ -16,54 +16,70 @@ if (window) {
         window.fluidPlayer = fluidPlayerInitializer;
     }
 
-    // Find all video tags marked as outstream and dynamically add player
-    document.querySelectorAll('[data-outstream]').forEach(function (e) {
-        var player = fluidPlayer(e, {
-            layoutControls: {
-                mute: true,
-                autoPlay: true,
-                controlBar: {
-                    autoHideTimeout: 0.5,
-                },
-            },
-            vastOptions: {
-                skipButtonCaption: '',
-                skipButtonClickCaption: '',
-                adCTAText: '',
-                fillToContainer: true,
-                vastStatic: true,
-                vastWithCredentials: false,
-                outstream: true,
-                adList: [
-                    {
-                        // Outstream is always pre roll
-                        roll: 'preRoll',
-                        // Fetch static VAST from tag
-                        vastTag: e.dataset.outstream,
+    window.setupOutstreamPlayer = function () {
+        // Find all video tags marked as outstream and dynamically add player
+        document.querySelectorAll('[data-outstream]').forEach(function (e) {
+            var player = fluidPlayer(e, {
+                layoutControls: {
+                    mute: true,
+                    autoPlay: true,
+                    controlBar: {
+                        autoHideTimeout: 0.5,
                     },
-                ],
-            },
-        });
+                },
+                vastOptions: {
+                    skipButtonCaption: '',
+                    skipButtonClickCaption: '',
+                    adCTAText: '',
+                    fillToContainer: true,
+                    vastStatic: true,
+                    vastWithCredentials: false,
+                    outstream: true,
+                    adList: [
+                        {
+                            // Outstream is always pre roll
+                            roll: 'preRoll',
+                            // Fetch static VAST from tag
+                            vastTag: e.dataset.outstream,
+                        },
+                    ],
+                },
+            });
 
-        // Toggle play if player is at least 50% visible
-        var observer = new IntersectionObserver(
-            function (entries) {
-                entries.forEach(function (entry) {
-                    if (entry.intersectionRatio >= 0.5) {
-                        if (!self.isPlayingMedia) {
-                            player.play();
+            // Toggle play if player is at least 50% visible
+            var observer = new IntersectionObserver(
+                function (entries) {
+                    entries.forEach(function (entry) {
+                        if (entry.intersectionRatio >= 0.5) {
+                            if (!self.isPlayingMedia) {
+                                player.play();
+                            }
                         }
-                    }
 
-                    if (entry.intersectionRatio == 0) {
-                        player.pause();
-                    }
-                });
-            },
-            {
-                threshold: [0.0, 0.5],
-            },
-        );
-        observer.observe(e);
-    });
+                        if (entry.intersectionRatio == 0) {
+                            player.pause();
+                        }
+                    });
+                },
+                {
+                    threshold: [0.0, 0.5],
+                },
+            );
+            observer.observe(e);
+        });
+    };
+
+    window.createOutstreamPlayer = function (el, vastXml, width, height) {
+      var videoPlayer = document.createElement('video');
+      videoPlayer.preload = true;
+      videoPlayer.autoplay = true;
+      videoPlayer.muted = true;
+      videoPlayer.style = `width:100%;max-width:${width}px;max-height:${height}px`;
+      videoPlayer.dataset.outstream = btoa(vastXml);
+      el.appendChild(videoPlayer);
+
+      window.setupOutstreamPlayer();
+    }
+
+    window.setupOutstreamPlayer();
 }
